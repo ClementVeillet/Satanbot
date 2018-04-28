5be5f18bca8cea0bf2ac2fd7004d3cdaf99696df
ARKAGEDON <38785328+ARKAGEDON@users.noreply.github.com>
2018-04-28 10:42:58 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+var prefix = '§'
+
+bot.on('ready', function () {
+    bot.user.setGame('THE HELL');
+    console.log("Connected");
+});
+
+bot.on('message', function (message)
+{
+if (message.content.startsWith(prefix + 'help')){
+	message.delete()
+	message.channel.send('**Voici la liste des commandes:**\n **§help**: donne la liste des commandes \n **§invite**: donne un lien d invitation pour le discord \n **§avatar**: pour voir votre avatar \n **§ping**: pour voir votre ping')
+}
+if (message.content.startsWith(prefix + 'invite')){
+	message.delete()
+	message.reply('Le lien du discord: https://discord.gg/6G2XsXT')
+}
+ if (message.content.startsWith(prefix + 'avatar')){
+ 	message.delete()
+ 	 message.channel.send(message.author.avatarURL)
+}
+if (message.content.startsWith(prefix + 'ping')){
+	message.delete()
+	message.reply('**Pong :ping_pong:**')
+}
+  if (message.content.startsWith(prefix + 'test')){
+  	  message.delete()
+      message.channel.send('Ok :white_check_mark:')
+}
+});
+bot.on('guildMemberAdd', function (member)
+{
+    member.createDM().then(function (channel) 
+    {
+        channel.send('Bienvenue sur le serveur **The Hell** ici on parle de tous ce qui concerne les jeux vidéos et autres amuse toi bien :)')
+    })
+});
+
+bot.login('NDM5MTY2MjI4NDI3MTc4MDA0.DcRqOw.iV6u3XUZ9qZH7gJUXSi4NZZ_g2s')
